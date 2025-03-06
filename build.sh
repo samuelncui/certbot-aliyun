5b8c088dc792263e4c837e52aed17278cd9fdd24
Samuel N Cui <cuijingning@bytedance.com>
2025-03-06 20:00:28 +0800

    feat: add pip update

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -xe;
 
+CURDIR=$(cd $(dirname $0); pwd);
+cd ${CURDIR};
+
+pip install -U pip setuptools;
 curl -sSL https://install.python-poetry.org | python3 -;
 poetry install;
